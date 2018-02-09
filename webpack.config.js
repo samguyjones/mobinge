bdc8c939d6ae42319610df1d25967dac7cab73cc
Sam Jones <samguyjones@gmail.com>
2018-02-08 18:53:43 -0600

    Possibly fixed basic build issue.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/main.js',
+  entry: './dist/mobinge.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
