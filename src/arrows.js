ad9beb191403867ee21794f86938b63e40548466
Sam Jones <samguyjones@gmail.com>
2018-07-31 22:47:10 -0500

    Updated arrows to use classname instead of class

--- src/arrows.js
+++ src/arrows.js
@@ -17,12 +17,12 @@ export default class Arrows extends React.Component
     return(
       <div key="arrows" style={barStyle}>
         <span style={leftStyle}>
-        <a onClick={this.props.onFirst} class="first-arrow"><img src="images/first-arrow.png"/></a>
-          <a onClick={this.props.onBack} class="back-arrow"><img src="images/back-arrow.png"/></a>
+        <a onClick={this.props.onFirst} className="first-arrow"><img src="images/first-arrow.png"/></a>
+          <a onClick={this.props.onBack} className="back-arrow"><img src="images/back-arrow.png"/></a>
         </span>
         <span style={rightStyle}>
-          <a onClick={this.props.onNext} class="next-arrow"><img src="images/next-arrow.png"/></a>
-          <a onClick={this.props.onLast} class="last-arrow"><img src="images/last-arrow.png"/></a>
+          <a onClick={this.props.onNext} className="next-arrow"><img src="images/next-arrow.png"/></a>
+          <a onClick={this.props.onLast} className="last-arrow"><img src="images/last-arrow.png"/></a>
         </span>
       </div>
     );
