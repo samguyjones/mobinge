8490b0dd67c4714505823a117ddb03ac76355b63
Sam Jones <samguyjones@gmail.com>
2018-10-01 23:10:56 -0500

    Updated main.js to have adjustable title page.

--- src/main.js
+++ src/main.js
@@ -8,18 +8,36 @@ import {
   Link
 } from 'react-router-dom'
 
-
 const Reader = ({match}) => {
+  const headerStyle={
+    width: '320px',
+    height: '60px',
+    backgroundColor: 'black'
+  };
   const panelNo = (match.params.panelNo) ? match.params.panelNo : 0;
   return <div>
+      <div id="header" style={headerStyle}>
+      <Link to='/index'>
+          <img src="/images/entry-list.png"/>
+      </Link>
+      <img src="/images/title.png"/>
+      </div>
       <Mobinge width="320" maxPanel="4" arrowThreshold="2" startPanel={panelNo}
       manifest="manifest.json"/>
     </div>;
 }
 
 const Index = () => {
+  const headerStyle={
+    width: '384px',
+    height: '60px',
+    backgroundColor: 'black'
+  };
   return <div>
-      <EntrySet/>
+  <div id="header" style={headerStyle}>
+  <img src="/images/entry-list-disabled.png"/>
+  <img src="/images/title.png"/>
+  </div>      <EntrySet/>
   </div>;
 }
 
